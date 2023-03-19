8ae34d30b68b8bf7a5645d6ea6f5eadf2f6798c3
Sompote R <sompote.r@gmail.com>
2023-03-19 13:51:29 +0700

    api health

--- app/pages/api/health.ts
+++ app/pages/api/health.ts
@@ -1,6 +1,4 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { db } from 'lib/firebaseUtil';
-import { withAuth } from 'middleware';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'GET') {
